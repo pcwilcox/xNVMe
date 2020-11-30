4fdc70f32ebd72c1ff8848d8db10c8572f1b98fb
Simon A. F. Lund <simon.lund@samsung.com>
2020-11-30 13:01:43 +0100

    tests:znd_explicit_open: make use of xnvme_cmd_ctx_from_dev()
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- tests/znd_explicit_open.c
+++ tests/znd_explicit_open.c
@@ -11,6 +11,7 @@ static int
 test_open_zdptr(struct xnvmec *cli)
 {
 	struct xnvme_dev *dev = cli->args.dev;
+	struct xnvme_cmd_ctx ctx = xnvme_cmd_ctx_from_dev(dev);
 	struct xnvme_spec_idfy_ns *nvm = (void *)xnvme_dev_get_ns(dev);
 	struct xnvme_spec_znd_idfy_ns *zns = (void *)xnvme_dev_get_ns_css(dev);
 	uint32_t nsid = xnvme_dev_get_nsid(dev);
@@ -22,7 +23,7 @@ test_open_zdptr(struct xnvmec *cli)
 	uint64_t zidx = 0;
 
 	struct xnvme_znd_report *before = NULL, *after = NULL;
-	struct xnvme_cmd_ctx req = {0 };
+
 	int err;
 
 	if (!zde_nbytes) {
@@ -83,8 +84,8 @@ test_open_zdptr(struct xnvmec *cli)
 	xnvme_spec_znd_descr_pr(XNVME_ZND_REPORT_DESCR(before, zidx), XNVME_PR_DEF);
 
 	err = xnvme_znd_mgmt_send(dev, nsid, zslba, XNVME_SPEC_ZND_CMD_MGMT_SEND_RESET, 0x0, NULL,
-				  XNVME_CMD_SYNC, &req);
-	if (err || xnvme_cmd_ctx_cpl_status(&req)) {
+				  XNVME_CMD_SYNC, &ctx);
+	if (err || xnvme_cmd_ctx_cpl_status(&ctx)) {
 		xnvmec_pinf("xnvme_znd_mgmt_send(RESET)");
 		err = err ? err : -EIO;
 		goto exit;
@@ -93,8 +94,8 @@ test_open_zdptr(struct xnvmec *cli)
 	xnvmec_pinf("Sending MGMT-EOPEN");
 
 	err = xnvme_znd_mgmt_send(dev, nsid, zslba, XNVME_SPEC_ZND_CMD_MGMT_SEND_OPEN, 0, zde,
-				  XNVME_CMD_SYNC, &req);
-	if (err || xnvme_cmd_ctx_cpl_status(&req)) {
+				  XNVME_CMD_SYNC, &ctx);
+	if (err || xnvme_cmd_ctx_cpl_status(&ctx)) {
 		xnvmec_pinf("xnvme_cmd_zone_mgmt(OPEN)");
 		err = err ? err : -EIO;
 		goto exit;
