216a33261dd0b3aefe0e187922f1318991ad2b06
Simon A. F. Lund <simon.lund@samsung.com>
2020-12-01 11:05:43 +0100

    cmd: have sgl-helper use context instead of device
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- src/xnvme_cmd.c
+++ src/xnvme_cmd.c
@@ -10,40 +10,39 @@
  * Calling this requires that opts at least has `XNVME_CMD_SGL_DATA`
  */
 static inline void
-xnvme_sgl_setup(struct xnvme_dev *dev, struct xnvme_spec_cmd *cmd, void *data, void *meta,
-		int opts)
+xnvme_sgl_setup(struct xnvme_cmd_ctx *ctx, void *data, void *meta, int opts)
 {
 	struct xnvme_sgl *sgl = data;
 	uint64_t phys;
 
-	cmd->common.psdt = XNVME_SPEC_PSDT_SGL_MPTR_CONTIGUOUS;
+	ctx->cmd.common.psdt = XNVME_SPEC_PSDT_SGL_MPTR_CONTIGUOUS;
 
 	if (sgl->ndescr == 1) {
-		cmd->common.dptr.sgl = sgl->descriptors[0];
+		ctx->cmd.common.dptr.sgl = sgl->descriptors[0];
 	} else {
-		xnvme_buf_vtophys(dev, sgl->descriptors, &phys);
+		xnvme_buf_vtophys(ctx->dev, sgl->descriptors, &phys);
 
-		cmd->common.dptr.sgl.unkeyed.type = XNVME_SPEC_SGL_DESCR_TYPE_LAST_SEGMENT;
-		cmd->common.dptr.sgl.unkeyed.len = sgl->ndescr * sizeof(struct xnvme_spec_sgl_descriptor);
-		cmd->common.dptr.sgl.addr = phys;
+		ctx->cmd.common.dptr.sgl.unkeyed.type = XNVME_SPEC_SGL_DESCR_TYPE_LAST_SEGMENT;
+		ctx->cmd.common.dptr.sgl.unkeyed.len = sgl->ndescr * sizeof(struct xnvme_spec_sgl_descriptor);
+		ctx->cmd.common.dptr.sgl.addr = phys;
 	}
 
 	if ((opts & XNVME_CMD_UPLD_SGLM) && meta) {
 		sgl = meta;
 
-		xnvme_buf_vtophys(dev, sgl->descriptors, &phys);
+		xnvme_buf_vtophys(ctx->dev, sgl->descriptors, &phys);
 
-		cmd->common.psdt = XNVME_SPEC_PSDT_SGL_MPTR_SGL;
+		ctx->cmd.common.psdt = XNVME_SPEC_PSDT_SGL_MPTR_SGL;
 
 		if (sgl->ndescr == 1) {
-			cmd->common.mptr = phys;
+			ctx->cmd.common.mptr = phys;
 		} else {
 			sgl->indirect->unkeyed.type = XNVME_SPEC_SGL_DESCR_TYPE_LAST_SEGMENT;
 			sgl->indirect->unkeyed.len = sgl->ndescr * sizeof(struct xnvme_spec_sgl_descriptor);
 			sgl->indirect->addr = phys;
 
-			xnvme_buf_vtophys(dev, sgl->indirect, &phys);
-			cmd->common.mptr = phys;
+			xnvme_buf_vtophys(ctx->dev, sgl->indirect, &phys);
+			ctx->cmd.common.mptr = phys;
 		}
 	}
 }
@@ -89,7 +88,7 @@ xnvme_cmd_pass(struct xnvme_dev *dev, struct xnvme_cmd_ctx *ctx, void *dbuf, siz
 	const int cmd_opts = opts & XNVME_CMD_MASK;
 
 	if ((cmd_opts & XNVME_CMD_MASK_UPLD) && dbuf) {
-		xnvme_sgl_setup(dev, &ctx->cmd, dbuf, mbuf, opts);
+		xnvme_sgl_setup(ctx, dbuf, mbuf, opts);
 	}
 
 	switch (cmd_opts & XNVME_CMD_MASK_IOMD) {
@@ -116,7 +115,7 @@ xnvme_cmd_pass_admin(struct xnvme_dev *dev, struct xnvme_cmd_ctx *ctx, void *dbu
 	}
 
 	if ((opts & XNVME_CMD_MASK_UPLD) && dbuf) {
-		xnvme_sgl_setup(dev, &ctx->cmd, dbuf, mbuf, opts);
+		xnvme_sgl_setup(ctx, dbuf, mbuf, opts);
 	}
 
 	return dev->be.sync.cmd_admin(dev, ctx, dbuf, dbuf_nbytes, mbuf, mbuf_nbytes, opts);
